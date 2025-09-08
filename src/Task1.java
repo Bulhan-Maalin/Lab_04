a7e0153af7118b462a15a6e710eae070454cb812
622140 <maalin.622140@swl.k12.oh.us>
2025-09-08 08:54:58 -0400

    test

--- src/Task1.java
+++ src/Task1.java
@@ -1,5 +1,13 @@
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("Hello world - this is a test.");
+       int intOperandA = 27;
+       int intOperandB = 3;
+       int intSum = intOperandA + intOperandB;
+       int intDifference = intOperandA - intOperandB;
+       int intProduct = intOperandA * intOperandB;
+       int intQuotient = intOperandA/intOperandB;
+       int intModulo = intOperandA%intOperandB;
+       double doubleOperandA;
+       dobule doubleO
     }
 }
