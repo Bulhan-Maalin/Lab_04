28c39ad0e6192698c423cea1b85f5876732bd3cf
622140 <maalin.622140@swl.k12.oh.us>
2025-09-09 08:43:02 -0400

    Finished Task 2

--- src/Task2.java
+++ src/Task2.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Task2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double userPrice;
+        double SALES_TAX = 0.05;
+
+        System.out.println("Please input the price of your purchase.");
+        userPrice = scan.nextDouble();
+        double totalPrice = (userPrice * SALES_TAX) + userPrice;
+        System.out.println("The total price of the purchase accounting for the 5% sales tax is $" + totalPrice + ".");
+
+
+    }
 }
