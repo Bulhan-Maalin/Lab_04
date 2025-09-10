0fa0b53dab3a440aa5ce0814cb494e5e558a545f
622140 <maalin.622140@swl.k12.oh.us>
2025-09-10 08:32:29 -0400

    Finished Task 3

--- src/Task3.java
+++ src/Task3.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class Task3 {
+    public static void main(String[] args) {
+        // initializing all of the variables & the scanner
+        Scanner scan = new Scanner(System.in);
+        double springCost;
+        double summerCost;
+        double fallCost;
+        double winterCost;
+        //receiving all of the costs from the user.
+        System.out.println("Please output the total cost of maintenance for spring:");
+        springCost = scan.nextDouble();
+        System.out.println("Please output the total cost of maintenance for summer:");
+        summerCost = scan.nextDouble();
+        System.out.println("Please output the total cost of maintenance for fall:");
+        fallCost = scan.nextDouble();
+        System.out.println("Please output the total cost of maintenance for winter:");
+        winterCost = scan.nextDouble();
+        //defining the total
+        double totalCost = springCost + summerCost + fallCost + winterCost;
+        //outputting the total
+        System.out.println("The total maintenance cost for all of the 4 seasons combined is $" + totalCost + "");
+    }
 }
