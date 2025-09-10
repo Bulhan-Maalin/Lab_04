27c895d3cc766af8ab5a7309eb9c56996e516c20
622140 <maalin.622140@swl.k12.oh.us>
2025-09-10 08:45:24 -0400

    Finished Task 4

--- src/Task4.java
+++ src/Task4.java
@@ -1,2 +1,13 @@
 public class Task4 {
+    public static void main(String[] args) {
+        double startingPayment = 5000;
+        double SALES_TAX = .17;
+        double monthOneMoney = (startingPayment * SALES_TAX) + startingPayment;
+        double monthTwoMoney = (monthOneMoney * SALES_TAX) + monthOneMoney;
+
+
+        System.out.println("The money in your account after one month is $" + monthOneMoney + ", and the money in your account after two months is $" + monthTwoMoney + "");
+
+
+    }
 }
