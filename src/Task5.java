3692679823630104f8b55d4680f7b25a23e52894
622140 <maalin.622140@swl.k12.oh.us>
2025-09-11 08:50:37 -0400

    Finished Task 5

--- src/Task5.java
+++ src/Task5.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class Task5 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double userTemperature;
+        double userTemperatureCelsius;
+
+        System.out.println("Please enter the temperature of your area in fahrenheit:");
+        userTemperature = scan.nextDouble();
+        userTemperatureCelsius = (double) 5/9 * (userTemperature - 32);
+        System.out.println("The temperature you inputted converted from fahrenheit to celsius is " + userTemperatureCelsius + ".");
+
+    }
 }
